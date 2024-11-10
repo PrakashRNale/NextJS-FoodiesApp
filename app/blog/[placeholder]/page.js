b64e79fd31a58ba9f80f21add83c30bd74d2cb38
PrakashRN <prakashcsedyp@gmail.com>
2024-11-10 22:39:03 +0530

    added basic routing

---
+++ app/blog/[placeholder]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogDetails = ({params}) => {
+  return (
+    <div>
+        <h1>Welcome to Blog {params.placeholder}</h1>
+    </div>
+  )
+}
+
+export default BlogDetails
