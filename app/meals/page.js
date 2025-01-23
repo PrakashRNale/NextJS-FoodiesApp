db1d0961cafae0f41114217c874f8822b227cb75
PrakashRN <prakashcsedyp@gmail.com>
2025-01-23 11:14:03 +0530

    Added meals grid component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import classes from './page.module.css';
+import Link from 'next/link';
+import MealsGrid from '@/components/meals/MealsGrid';
 
 const Meals = () => {
   return (
-    <div>
-        <div>Meals</div>
-       
-    </div>
+   <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created <span className={classes.highlight}> by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it your self</p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>
+            Share your favorite recipe
+          </Link>
+        </p>
+      </header>
+      <main className={classes.main}> 
+        <MealsGrid meals={[]} />
+      </main>
+   </>
   )
 }
 
