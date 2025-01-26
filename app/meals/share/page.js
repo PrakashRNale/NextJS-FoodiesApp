71e032cc2dc62b283ab577dbf34b89b2904041e4
PrakashRN <prakashcsedyp@gmail.com>
2025-01-26 13:48:34 +0530

    Added Input validation

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,14 @@
+'use client';
 import ImagePicker from '@/components/image-picker/ImagePicker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/serverActions';
+import FormStatusButton from '@/components/FormStatusButton/FormStatusButton';
+import { useFormState } from 'react-dom';
 
 export default function ShareMealPage() {
+
+  const [state, formActon] = useFormState(shareMeal, { message : null })
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +18,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={formActon}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,9 +46,10 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Yout Image" name="image" />
+          { state.message && <p>{ state.message }</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <FormStatusButton />
           </p>
         </form>
       </main>
